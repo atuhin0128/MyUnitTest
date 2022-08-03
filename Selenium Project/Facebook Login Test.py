e4b894c949ddac31a7f116409e5adad1a29abffc
atuhin0128 <atuhin0128@gmail.com>
2022-08-03 17:55:56 +0600

    My Unit Test

--- Selenium Project/Facebook Login Test.py
+++ Selenium Project/Facebook Login Test.py
@@ -1,38 +1,47 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from PIL import Image
+
 
 class FaceBookLogin(unittest.TestCase):
 
     def setUp(self):
         s = Service("D:\Chrome Driver\chromedriver.exe")
-        self.driver = webdriver.Chrome(service=s)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(service=s, options=chrome_options)
 
 
     def test_facebook_login(self):
         driver = self.driver
         driver.get("https://www.facebook.com/")
-        self.assertIn("Facebook", driver.title)
+        driver.implicitly_wait(5000)
+        self.assertIn("Facebook fd", driver.title)
         user_id = driver.find_element(By.NAME, "email")
-        user_id.send_keys("atuhin37@yahoo.com")
+        user_id.send_keys("test")
         user_pass = driver.find_element(By.NAME, "pass")
-        user_pass.send_keys("lovebird")
-        login_but = driver.find_element(By.NAME,"login")
-        login_but.submit()
-        driver.implicitly_wait(2000)
-        #elem.send_keys(Keys.RETURN)
-        #self.assertNotIn("No results found.", driver.page_source)
+        user_pass.send_keys("test")
+        login_but = driver.find_element(By.NAME, "login")
+        driver.implicitly_wait(5000)
+        login_but.click()
+
+
+
+        # elem.send_keys(Keys.RETURN)
+        # self.assertNotIn("No results found.", driver.page_source)
 
     def test_screenshots(self):
         driver = self.driver
         driver.save_screenshot("image.png")
         image = Image.open("image.png")
         image.show()
+
    # def tearDown(self):
-       # self.driver.close()
+     #   self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
